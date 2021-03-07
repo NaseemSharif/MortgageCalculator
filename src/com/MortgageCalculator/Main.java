3ae8cabc8dbebccd6110ca82106f62acde141312
NaseemSharif <67925146+NaseemSharif@users.noreply.github.com>
2021-03-07 23:05:49 +0000

    created algorithm, now user can calculate their mortgage payment

--- src/com/MortgageCalculator/Main.java
+++ src/com/MortgageCalculator/Main.java
@@ -12,10 +12,14 @@ public class Main {
         int principal = scanner.nextInt();
         System.out.print("Annual Interest: ");
         float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / 100 * 12;
+        float monthlyInterest = annualInterest / 100 / 12;
         System.out.print("How Many Years: ");
         byte years = scanner.nextByte();
         int months = years * 12;
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,months)) /
+                (Math.pow(1+ monthlyInterest, months) - 1);
+        System.out.println("Your monthly payment is " + mortgage);
 
     }
 }
