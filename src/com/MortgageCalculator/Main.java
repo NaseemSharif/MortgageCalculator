4fce3b7e0a03fd53af5ace67794f2896bf7d4e53
NaseemSharif <67925146+NaseemSharif@users.noreply.github.com>
2021-03-07 23:15:39 +0000

    code more readable and result is formatted as currency

--- src/com/MortgageCalculator/Main.java
+++ src/com/MortgageCalculator/Main.java
@@ -1,25 +1,28 @@
 package com.MortgageCalculator;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
 
         System.out.println("Mortgage Calculator");
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
         System.out.print("Annual Interest: ");
         float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / 100 / 12;
-        System.out.print("How Many Years: ");
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        System.out.print("Period (Years): ");
         byte years = scanner.nextByte();
-        int months = years * 12;
+        int numberOfPayments = years * MONTHS_IN_YEAR;
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,months)) /
-                (Math.pow(1+ monthlyInterest, months) - 1);
-        System.out.println("Your monthly payment is " + mortgage);
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,numberOfPayments)) /
+                (Math.pow(1+ monthlyInterest, numberOfPayments) - 1);
+        System.out.println("Your Mortgage monthly payment is:   " + NumberFormat.getCurrencyInstance().format(mortgage));
 
     }
 }
