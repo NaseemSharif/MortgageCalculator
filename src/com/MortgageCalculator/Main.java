0a05532e6222f65907c0acf74959f6e2ec1c0720
NaseemSharif <67925146+NaseemSharif@users.noreply.github.com>
2021-03-07 22:37:15 +0000

    adding the code to recieve user inputs. just need to code the algorithm

--- src/com/MortgageCalculator/Main.java
+++ src/com/MortgageCalculator/Main.java
@@ -1,8 +1,21 @@
 package com.MortgageCalculator;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Mortgage Calculator");
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.print("Annual Interest: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / 100 * 12;
+        System.out.print("How Many Years: ");
+        byte years = scanner.nextByte();
+        int months = years * 12;
+
     }
 }
